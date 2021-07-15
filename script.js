002cc77d73cbad683ffa91ba394d601aede8a2a3
VishakhaVashisht <vashishtvishakha93@gmail.com>
2021-07-15 15:54:37 +0100

    ex-3,4 ,4

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function deleteClass(){
-    var div = document.getElementById('new');
+function deleteClassSection1(){
+    let div = document.getElementById('new');
     div.classList.remove('jumbotron');
+
+    let div1 = document.getElementById('favorite-album');
+    div1.classList.remove('badge');
+}
+function deleteClassSection2(){
+
+    let div1 = document.getElementById('favorite-album');
+    div1.classList.remove('badge');
 }
